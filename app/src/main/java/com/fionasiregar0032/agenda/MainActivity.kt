754a5cecb6916db6881e94c72a4120db322b9c17
fionas14 <siregarfiona90@gamil.com>
2025-05-09 20:44:49 +0700

    Menambahkan Top App Bar

--- app/src/main/java/com/fionasiregar0032/agenda/MainActivity.kt
+++ app/src/main/java/com/fionasiregar0032/agenda/MainActivity.kt
@@ -6,10 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.fionasiregar0032.agenda.ui.theme.AgendaTheme
 
@@ -19,15 +23,37 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AgendaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
             }
+            MainScreen()
         }
     }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+
+    Scaffold (
+        containerColor = Color(0xFFFFF5E1),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Agenda")
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = Color(0xFFFF69B4),
+                    titleContentColor = Color.Black,
+                )
+            )
+        }
+    ) { innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
+    } }
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier) {
+    Text(
+        text = "",
+        modifier = modifier
+    )
 }
 
 @Composable
